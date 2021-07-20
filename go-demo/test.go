1c2bea9830aab37a3e87387b88e179d3fb211f83
liwei24 <liwei24@100tal.com>
2021-07-20 18:45:19 +0800

    命令行

--- go-demo/test.go
+++ go-demo/test.go
@@ -3,18 +3,28 @@ package main
 import (
 	"github.com/pdfcpu/pdfcpu/pkg/api"
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
+	"os"
 )
 
 func main() {
+
 	oldFile := "./pdf.pdf"
 	newFile := "./output.pdf"
 	waterFile := "./water.png"
-
+	if (os.Args[1] != "") {
+		oldFile = os.Args[1]
+	}
+	if (os.Args[2] != "") {
+		newFile = os.Args[2]
+	}
+	if (os.Args[3] != "") {
+		waterFile = os.Args[3]
+	}
 	water(oldFile, newFile, waterFile)
 }
 
 func water(oldFile string, newFile string, waterFile string) {
 	onTop := true
-	wm, _ := pdfcpu.ParseImageWatermarkDetails(waterFile, "sc:4 abs, d:1, op:.4, pos:c", onTop, 1)
+	wm, _ := pdfcpu.ParseImageWatermarkDetails(waterFile, "sc:1 rel, rot: 0.0, op:.4, pos:c", onTop, 1)
 	api.AddWatermarksFile(oldFile, newFile, nil, wm, nil)
 }
